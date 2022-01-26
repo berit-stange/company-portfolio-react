7bdbf1db1716e13a031517eb83a1c62554b38e68
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-01-26 17:27:02 +0100

    removed default content

---
+++ Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+
+    return (
+        <div>
+            <div>LOGO</div>
+            <ul>
+                <Link>Link</Link>
+                <Link>Link</Link>
+                <Link>Link</Link>
+            </ul>
+        </div>
+    )
+}
