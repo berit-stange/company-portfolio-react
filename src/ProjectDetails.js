6e135ff4b842520643d1a66fa0df99d617f24b64
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-01-27 16:22:35 +0100

    deleted unused code

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -4,27 +4,6 @@ import paperlove1 from "./paperlove1.jpg";
 function ProjectDetails() {
     return (
         <div>
-            {/* <div className="grid-container-2">
-                <div className="image-large">
-                    <img src={paperlove1}></img>
-                </div>
-                <div>
-                    <h1>Projekt Titel</h1>
-                    <h2>Ort</h2>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                        magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                        et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
-                        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-                        sanctus est Lorem ipsum dolor sit amet.</p>
-                    <h3>Kunde: Lorem ipsum dolor</h3>
-                    <h3>Jahr: 2014</h3>
-                </div>
-            </div> */}
             <div className="grid-container-mix">
 
                 <div className="image-medium grid-box">
