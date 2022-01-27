b83a89be086f891571765b4b0fb5223b00718426
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-01-27 16:18:05 +0100

    css changes: grid in ProjectDetails component

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 
 function Footer() {
     return (
         <div className="footer-container">
+            <Link to="/">
+                <img src={logo} className="logo"></img>
+            </Link>
 
+            <div>
+                <ul>
+                    <Link to="/projekte">
+                        <li>Link</li>
+                    </Link>
+                    <Link to="/projekte">
+                        <li>Link</li>
+                    </Link>
+                    <Link to="/projekt-details">
+                        <li>Link</li>
+                    </Link>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,10 +1,13 @@
 import React from "react";
+import paperlove1 from "./paperlove1.jpg";
 
 function ProjectDetails() {
     return (
         <div>
-            <div className="grid-container-2">
-                <div className="image-large"></div>
+            {/* <div className="grid-container-2">
+                <div className="image-large">
+                    <img src={paperlove1}></img>
+                </div>
                 <div>
                     <h1>Projekt Titel</h1>
                     <h2>Ort</h2>
@@ -20,13 +23,52 @@ function ProjectDetails() {
                         sanctus est Lorem ipsum dolor sit amet.</p>
                     <h3>Kunde: Lorem ipsum dolor</h3>
                     <h3>Jahr: 2014</h3>
-                    {/* <p>Lorem ipsum dolor</p> */}
                 </div>
-            </div>
-            <div className="grid-container-3">
-                <div className="image-medium"></div>
-                <div className="image-medium"></div>
-                <div className="image-medium"></div>
+            </div> */}
+            <div className="grid-container-mix">
+
+                <div className="image-medium grid-box">
+                    {/* <img src={paperlove1}></img> */}
+                </div>
+                <div className="image-medium grid-box">
+                    {/* <img src={paperlove1}></img> */}
+                </div>
+                <div className="image-medium grid-box">
+                    {/* <img src={paperlove1}></img> */}
+                </div>
+                <div className="image-medium grid-box">
+                    {/* <img src={paperlove1}></img> */}
+                </div>
+
+                <div className="text-medium grid-box-text">
+                    <h1>T1: Projekt Titel</h1>
+                    <h2>Ort</h2>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                        magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                        et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+                        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+                        sanctus est Lorem ipsum dolor sit amet.</p>
+                    <h3>Kunde: Lorem ipsum dolor</h3>
+                    <h3>Jahr: 2014</h3>
+                </div>
+                <div className="text-medium grid-box-text2">
+                    <h3>T1</h3>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                        magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+                        et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+                        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+                        sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
             </div>
         </div>
     )
