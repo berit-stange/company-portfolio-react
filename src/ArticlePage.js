da137bfee2890f14c6754d2ee1057a39e1ccef5b
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-02-02 00:05:47 +0100

    css: grid ArticlePage font

--- src/ArticlePage.js
+++ src/ArticlePage.js
@@ -27,14 +27,11 @@ function ArticlePage() {
                 <div className="grid-box-a-text1">
                     <h1>Artikelseite</h1>
                     <h2>Text 1</h2>
-                    <p>Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien
+                    <p className="article-intro">Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien
                         leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste
                         des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden
                         fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien.
-
                     </p>
-                    <h3>Kunde: Lorem ipsum dolor</h3>
-                    <h3>Jahr: 2014</h3>
                 </div>
 
                 <div className="grid-box-a-text2">
