446050f504caed4f2764644211816e3085d76587
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-02-10 23:33:57 +0100

    added class for h1

--- src/ProjectsSelection.js
+++ src/ProjectsSelection.js
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectsSelection() {
     return (
         <div>
-            <h1>
+            <h1 className="title-center">
                 COMPANY PORTFOLIO
             </h1>
             <div className="center">
