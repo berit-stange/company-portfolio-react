0d010dbfbc7d7625436f05d8bfadc6e05f5204a9
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-02-17 23:21:32 +0100

    moved list of projects into a table

--- src/CMSallPages.js
+++ src/CMSallPages.js
@@ -13,27 +13,47 @@ function CMSallPages() {
 
                 <div className="overview-row">
                     <p>Projekt 1</p>
-                    <span className="material-icons-round icon-settings">settings</span>
+                    <p>Ausstellung</p>
+                    <span className="material-icons-round">settings</span>
                 </div>
 
                 <div className="overview-row">
                     <p>Projekt 2</p>
-                    <span className="material-icons-round icon-settings">settings</span>
+                    <p>Shop</p>
+                    <span className="material-icons-round">settings</span>
                 </div>
 
                 <div className="overview-row">
                     <p>Projekt 3</p>
-                    <span className="material-icons-round icon-settings">settings</span>
+                    <p>Ausstellung</p>
+                    <span className="material-icons-round">settings</span>
                 </div>
 
-                <div className="overview-row">
-                    <p>Projekt 4</p>
-                    <span className="material-icons-round icon-settings">settings</span>
-                </div>
 
-                <div className="overview-row">
-                    <p>Projekt 5</p>
-                    <span className="material-icons-round icon-settings">settings</span>
+                <div id="overview-table">
+                    <table>
+                        <tr>
+                            <th>Title</th>
+                            <th>Type</th>
+                            <th>Action</th>
+                        </tr>
+                        <tr>
+                            <td>Projekt 1</td>
+                            <td>Ausstellung</td>
+                            <td>
+                                <span className="material-icons-round">settings</span>
+                                <span className="material-icons-round">delete</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Projekt 2</td>
+                            <td>Shop</td>
+                            <td>
+                                <span className="material-icons-round">settings</span>
+                                <span className="material-icons-round">delete</span>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
 
             </div>
