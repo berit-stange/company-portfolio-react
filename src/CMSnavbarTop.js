b55897a6a342025d09f88705c6aa1ecc3379fb1a
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-03-04 19:06:29 +0100

    created CMSnavbarTop component

---
+++ src/CMSnavbarTop.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CMSnavbarTop() {
+
+    return (
+        <div>
+
+            <div className="admin-navbar-top">
+                <ul>
+                    <Link to="#">
+                        <li>Link 1</li>
+                    </Link>
+
+                    <Link to="#">
+                        <li>Link 2</li>
+                    </Link>
+
+                    <Link to="#">
+                        <li>Link 3</li>
+                    </Link>
+
+                    <Link to="/">
+                        <li>HOME</li>
+                    </Link>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default CMSnavbarTop;
