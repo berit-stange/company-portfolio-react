26c47485d22c24d0f9fd4e7f82d293b3d898cbe3
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-05-19 22:14:56 +0200

    added Footer component

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import ImageSlider from './ImageSlider';
 import ProjectsSelection from './ProjectsSelection';
 import Navigation from './Navigation';
+import Footer from './Footer';
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
             <Navigation />
             <ImageSlider />
             <ProjectsSelection />
+            <Footer />
         </div>
     )
 }
