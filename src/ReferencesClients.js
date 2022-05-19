6df2451c40ccafb302cfd09cbb7ca48de218bca8
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-05-19 22:15:22 +0200

    added Footer component

--- src/ReferencesClients.js
+++ src/ReferencesClients.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from './Navigation';
 import { Link } from 'react-router-dom';
+import Footer from "./Footer";
 
 function ReferencesClients() {
     return (
@@ -62,6 +63,8 @@ function ReferencesClients() {
                     </div>
                 </div>
             </div>
+
+            <Footer />
         </div>
 
     )
