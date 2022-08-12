463955112dc4083327ed615826d65d2564831f81
berit-stange <76659950+berit-stange@users.noreply.github.com>
2022-08-12 19:04:34 +0200

    created user navigation, added logout button + user welcome

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 
 import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 function Navigation() {
+
+    const [user] = useAuthState(auth);
 
     // Signout function
     const logout = () => {
@@ -13,29 +16,39 @@ function Navigation() {
 
     return (
         <div className="nav-container">
-            <div>
+
+            <div className="user-navigation">
                 <button className="btn-logout" onClick={logout}>Logout</button>
+
+                <div className="welcome">
+                    Hallo {auth.currentUser.email}!
+                </div>
+
             </div>
 
-            <Link to="/">
-                <img src={logo} className="logo" alt="Logo company xy"></img>
-            </Link>
+            <div className="page-navigation">
 
-            <div>
-                <ul>
-                    <Link to="/projekte">
-                        <li>Projekte</li>
-                    </Link>
-                    <Link to="/projekt-details">
-                        <li>Details</li>
-                    </Link>
-                    <Link to="/artikel">
-                        <li>Artikel</li>
-                    </Link>
-                    <Link to="/referenzen">
-                        <li>Referenzen</li>
-                    </Link>
-                </ul>
+                <Link to="/">
+                    <img src={logo} className="logo" alt="Logo company xy"></img>
+                </Link>
+
+                <div>
+                    <ul>
+                        <Link to="/projekte">
+                            <li>Projekte</li>
+                        </Link>
+                        <Link to="/projekt-details">
+                            <li>Details</li>
+                        </Link>
+                        <Link to="/artikel">
+                            <li>Artikel</li>
+                        </Link>
+                        <Link to="/referenzen">
+                            <li>Referenzen</li>
+                        </Link>
+                    </ul>
+                </div>
+
             </div>
 
         </div>
