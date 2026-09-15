38ec328cfe4f12e2f650bc8ed89a62bc09ac2d6f
berit-stange <76659950+berit-stange@users.noreply.github.com>
2026-09-15 22:34:19 +0200

    auslesen der collection und zurückgeben in der Komponente

--- src/ProjectsOverview.js
+++ src/ProjectsOverview.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from './Navigation';
 import { Link } from 'react-router-dom';
 import Footer from "./Footer";
 
+import { auth, db } from "./firebase/config"; 
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { collection, getDocs, query, where } from "firebase/firestore";
+
 function ProjectsOverview() {
+    
+    const [user] = useAuthState(auth); 
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => { if (!user) return; 
+        const projectsCollection = collection(db, "projects"); 
+        const q = query( 
+            projectsCollection, 
+            where("uid", "==", user.uid) ); 
+        
+            getDocs(q).then((snapshot) => { 
+                const projectData = snapshot.docs.map((doc) => ({ 
+                    id: doc.id, 
+                    ...doc.data() })); 
+                    setProjects(projectData); 
+                });
+                 }, [user]);
+
     return (
         <div>
             <Navigation />
@@ -16,18 +39,21 @@ function ProjectsOverview() {
                     <button className="btn-filter">Display</button>
                     <button className="btn-filter">Messe</button>
                 </div>
+
                 <div className="center">
                     <div className="project-grid-container">
-                        <Link to="/projekt-details">
-                            <div className="project-image-small"></div>
+
+                        {projects.map((project) => (
+                        <Link to="/projekt-details" key={project.id}>
+                            <div className=""> 
+                            <h2>{project.title}</h2> 
+                            <p>{project.category}</p> 
+                            </div>
                         </Link>
-                        <Link to="/projekt-details">
-                            <div className="project-image-small"></div>
-                        </Link>
-                        <Link to="/projekt-details">
-                            <div className="project-image-small"></div>
-                        </Link>
-                        <div className="project-image-small"></div>
+                        
+))}
+
+                        {/* <div className="project-image-small"></div>
                         <div className="project-image-small"></div>
                         <div className="project-image-small"></div>
 
@@ -41,7 +67,7 @@ function ProjectsOverview() {
 
                         <div className="project-image-small"></div>
                         <div className="project-image-small"></div>
-                        <div className="project-image-small"></div>
+                        <div className="project-image-small"></div> */}
                     </div>
                 </div>
             </div>
@@ -49,7 +75,7 @@ function ProjectsOverview() {
             <Footer />
         </div>
 
-    )
+    );
 }
 
 export default ProjectsOverview;
